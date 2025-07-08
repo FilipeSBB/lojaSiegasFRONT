2b88f045b04739f6717c851eb7738cc7b908bdd6
Filipe Barbosa <filipe_barbosaa@hotmail.com>
2025-07-08 03:07:21 -0300

    please work

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '"https://lojasiegasbackend.onrender.com"',
+    baseURL: 'https://lojasiegasbackend.onrender.com',
 });
 
 export const loginUser = async (email, senha) => {
